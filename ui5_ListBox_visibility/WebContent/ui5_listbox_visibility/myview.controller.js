55840a42b9f1bd62c286bf687081e65750c92ba0
scollenburg <scollenburg@aspirehr.com>
2014-05-07 23:11:02 -0500

    This version includes a workaround for the issue. You can disable the workaround interactively using a toggle button.

--- ui5_ListBox_visibility/WebContent/ui5_listbox_visibility/myview.controller.js
+++ ui5_ListBox_visibility/WebContent/ui5_listbox_visibility/myview.controller.js
@@ -1,25 +1,56 @@
 sap.ui.controller("ui5_listbox_visibility.myview", {
 
 	mdata : {
-		uistate : { showshapes : true, showcolors : true },
+		uistate : {
+			showshapes : true,
+			showcolors : true,
+			enableWorkaround: false,
+		},
 		viewdata : {
 			formname : "Visibility testbed",
-			shape_list : [
-					{ id : "0", text : "Circle" },
-					{ id : "1", text : "Point" },
-					{ id : "2", text : "Line" },
-					{ id : "3", text : "Triange" },
-					{ id : "4", text : "Square" } ],
-			color_list : [
-					{ id : "650", text : "Red" },
-					{ id : "590", text : "Orange" },
-					{ id : "589", text : "Yellow" },
-					{ id : "510", text : "Green" },
-					{ id : "475", text : "Blue" },
-					{ id : "445", text : "Indigo" },
-					{ id : "400", text : "Violet" } ]
+			shape_list : [ {
+				id : "0",
+				text : "Circle"
+			}, {
+				id : "1",
+				text : "Point"
+			}, {
+				id : "2",
+				text : "Line"
+			}, {
+				id : "3",
+				text : "Triange"
+			}, {
+				id : "4",
+				text : "Square"
+			} ],
+			color_list : [ {
+				id : "650",
+				text : "Red"
+			}, {
+				id : "590",
+				text : "Orange"
+			}, {
+				id : "589",
+				text : "Yellow"
+			}, {
+				id : "510",
+				text : "Green"
+			}, {
+				id : "475",
+				text : "Blue"
+			}, {
+				id : "445",
+				text : "Indigo"
+			}, {
+				id : "400",
+				text : "Violet"
+			} ]
 
-		} },
+		}
+	},
+
+	oHidingHandler : false,
 
 	/**
 	 * Called when a controller is instantiated and its View controls (if
@@ -54,9 +85,15 @@ sap.ui.controller("ui5_listbox_visibility.myview", {
 	 * 
 	 * @memberOf ui5_listbox_visibility.myview
 	 */
-	// onAfterRendering: function() {
-	//
-	// },
+	onAfterRendering : function() {
+
+		/*
+		 * Nice in theory, however this does *not* get called during rerendering
+		 */
+		// for (var c in this.toBeHidden) {
+		// console.log(typeof(this.toBeHidden[c]));
+		// }
+	},
 	/**
 	 * Called when the Controller is destroyed. Use this one to free resources
 	 * and finalize activities.
@@ -78,5 +115,35 @@ sap.ui.controller("ui5_listbox_visibility.myview", {
 		this.oModel.refresh();
 
 	},
+
+	getHider : function() {
+		if (!this.oHidingHandler) {
+			var o = {
+				onAfterRendering : function(oEvent) {
+
+					var bFixIt = this.getModel().getProperty("/uistate/enableWorkaround");
+					
+					var oSAPDom = this.$();
+					/*
+					 * We can't query using ID, since 
+					 */
+					var oTooMany = $("[data-sap-ui~=" + this.getId() + "]");
+
+					if (bFixIt && oTooMany.length > 1) {
+						for (var j = 0; j < oTooMany.length; j++) {
+							// console.log("many[" + j + "]: "
+							// + (oDom === many[j]));
+							if (oTooMany[j].parentElement != oSAPDom[0].parentElement)
+								oTooMany[j].remove();
+						}
+					}
+				}
+			};
+			this.oHidingHandler = o;
+		}
+		// console.log(typeof (this.oHidingHandler));
+
+		return this.oHidingHandler;
+	}
 
 });
--- ui5_ListBox_visibility/WebContent/ui5_listbox_visibility/myview.view.js
+++ ui5_ListBox_visibility/WebContent/ui5_listbox_visibility/myview.view.js
@@ -1,5 +1,8 @@
 sap.ui.jsview("ui5_listbox_visibility.myview", {
 
+	oC : false,
+	thisView : false,
+
 	/**
 	 * Specifies the Controller belonging to this View. In the case that it is
 	 * not implemented, or that "null" is returned, this View does not have a
@@ -46,6 +49,28 @@ sap.ui.jsview("ui5_listbox_visibility.myview", {
 			}
 		});
 		oVerticalLayout.addContent(oButton);
+
+		var oButton = new sap.ui.commons.Button(oController
+				.createId("benableWorkaround"), {
+			text : "Toggle /uistate/enableWorkaround",
+			buttonStyle : "Emph",
+			press : function() {
+				oController.toggleUIState("enableWorkaround");
+			}
+		});
+		oVerticalLayout.addContent(oButton);
+
+		// Horizontal for displaying the Workaround state
+		var oHorizontalLayout = new sap.ui.layout.HorizontalLayout();
+
+		oHorizontalLayout.addContent(new sap.ui.commons.Label({
+			text : "--> enableWorkaround: "
+		}));
+		oHorizontalLayout.addContent(new sap.ui.commons.Label({
+			text : "{/uistate/enableWorkaround}"
+		}));
+
+		oVerticalLayout.addContent(oHorizontalLayout);
 
 		// Give us a header
 		oVerticalLayout.addContent(new sap.ui.commons.TextView({
@@ -95,7 +120,17 @@ sap.ui.jsview("ui5_listbox_visibility.myview", {
 		 * If you comment out the next line, the drop-down will get no values.
 		 */
 		oCell.addContent(olbShapes);
-		
+
+		// Workaround...
+		olbShapes.addDelegate(this.oC.getHider(), false, olbShapes, false);
+
+		// olbShapes.attachEvent("onAfterRendering", olbShapes, function() {
+		// oC.onAfterRendering();
+		// });
+		// olbShapes.attachEvent("AfterRendering", olbShapes, function() {
+		// oC.onAfterRendering();
+		// });
+
 		oCell.addContent(oddShapes);
 		oRow.addCell(oCell);
 		oMatrix.addRow(oRow); // Shapes
@@ -117,7 +152,11 @@ sap.ui.jsview("ui5_listbox_visibility.myview", {
 		var oCell = new sap.ui.commons.layout.MatrixLayoutCell();
 		var olbColors = new sap.ui.commons.ListBox(oController
 				.createId("lbcolors"));
-
+		/*
+		 * This next line keeps the ListBox from showing, but the entire list is
+		 * not shown
+		 */
+		// olbColors.setVisible(false);
 		olbColors.bindAggregation("items", "/viewdata/color_list",
 				new sap.ui.core.ListItem({
 					key : "{id}",
@@ -132,6 +171,10 @@ sap.ui.jsview("ui5_listbox_visibility.myview", {
 
 		oCell.addContent(olbColors);
 		oCell.addContent(oddColors);
+
+		// Workaround...
+		olbColors.addDelegate(this.oC.getHider(), false, olbColors, false);
+
 		oRow.addCell(oCell);
 		oMatrix.addRow(oRow); // Colors
 
